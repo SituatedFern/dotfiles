fb2f99f6f67ff1d91cc7d0c0a672191e02297c68
SituatedFern <themareepevoloutions@gmail.com>
2022-12-06 17:54:31 -0500

    changed emacs appearance

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,7 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-
+(setq doom-font (font-spec :family "JetBrains Mono Nerd Font" :size 14)
+      doom-variable-pitch-font (font-spec :family "JetBrains Mono Nerd Font" :size 14))
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 (setq user-full-name "John Doe"
@@ -32,11 +33,11 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-dracula)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
